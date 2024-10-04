dc91e9ff17e5ddb183ed94f732f206d431f81109
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-04 23:23:11 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,187 @@
+class script(object):
+    START_TXT = """<b>𝖧𝖾𝗒 {}, 𝖨 𝖠𝗆 <a href=https://t.me/{}>{}</a>, 𝖧𝖺𝗉𝗉𝗒 🖤 𝖳𝗈 𝖧𝖺𝗏𝖾 𝖸𝗈𝗎</b>
+
+𝖨𝖺𝗆 𝖯𝗈𝗐𝖾𝗋𝖥𝗎𝗅𝗅 𝖠𝗎𝗍𝗈 𝖥𝗂𝗅𝗍𝖾𝗋 + 𝖬𝗈𝗏𝗂𝖾 𝖲𝖾𝖺𝗋𝖼𝗁 + 𝖬𝖺𝗇𝗎𝖺𝗅 𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍 ⚙
+
+𝖨𝖺𝗆 𝖠 𝖡𝗈𝗍 𝖯𝗋𝗈𝗏𝗂𝖽𝗂𝗇𝗀 𝖬𝗈𝗏𝗂𝖾𝗌 𝖠𝗇𝖽 𝖲𝖾𝗋𝗂𝖾𝗌 𝖲𝗈 𝖠𝖽𝖽 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖥𝖺𝗆. 𝖳𝗁𝖾𝗇 𝖨 𝖶𝗂𝗅𝗅 𝗌𝖾𝗇𝗍 𝖳𝗁𝖾 𝖬𝗈𝗏𝗂𝖾𝗌⏳
+
+𝖢𝗅𝗂𝖼𝗄 𝖡𝖾𝗅𝗈𝗐 𝖴𝗌𝖾𝖿𝗎𝗅 𝖡𝗎𝗍𝗍𝗈𝗇𝗌🫶"""
+    
+    HELP_TXT = """<b>𝖧𝖾𝗒 {}, 𝖨𝖺𝗆 𝖧𝖺𝗉𝗉𝗒 🖤 𝖳𝗈 𝖧𝖺𝗏𝖾 𝖸𝗈𝗎</b>
+    
+𝖶𝗂𝗍𝗁 𝖳𝗁𝗂𝗌 𝖧𝖾𝗅𝗉 𝖸𝗈𝗎 𝖶𝗂𝗅𝗅 𝖴𝗇𝖽𝖾𝗋𝗌𝗍𝖺𝗇𝖽 𝖧𝗈𝗐 𝖳𝗈 𝖴𝗌𝖾 𝖳𝗁𝗂𝗌 𝖡𝗈𝗍🏌️
+    
+𝖢𝗁𝖾𝖼𝗄 𝖡𝖾𝗅𝗈𝗐 𝖧𝖾𝗅𝗉 𝖥𝗎𝗇𝖼𝗍𝗂𝗈𝗇𝗌🤍 """
+    
+    ABOUT_TXT = """<b>𝖧𝖾𝗒 𝖡𝗋𝗈 𝖨𝖺𝗆 𝖧𝖺𝗉𝗉𝗒 🖤 𝖳𝗈 𝖧𝖺𝗏𝖾 𝖸𝗈𝗎</b>
+
+𝖨𝖺𝗆 𝖯𝗈𝗐𝖾𝗋𝖥𝗎𝗅𝗅 𝖠𝗎𝗍𝗈 𝖥𝗂𝗅𝗍𝖾𝗋 + 𝖬𝗈𝗏𝗂𝖾 𝖲𝖾𝖺𝗋𝖼𝗁 + 𝖬𝖺𝗇𝗎𝖺𝗅 𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍 ⚙
+
+𝖧𝗈𝗌𝗍𝖾𝖽 𝖡𝗒 @heyboy2k04 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝗂𝗇𝗀 𝖬𝖾 𝗂𝗇 𝖵𝖯𝖲. 𝖸𝗈𝗎 𝖢𝖺𝗇 𝖩𝗈𝗂𝗇 𝖠𝗇𝖽 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝗂𝗇 𝖬𝗈𝗏𝗂𝖾𝗌 𝖦𝗋𝗈𝗎𝗉 @color_films
+"""
+
+    SOURCE_TXT = """<b>കൊട്ക്ക്ണില്ല്യാ......</b>"""
+    MCAHU_TXT = """The new command features are listed below. More features coming soon
+    
+/start - Welcome🤗 (Help , About and others)
+    
+/setchat1 - 1st force Sub Channel ID
+
+/setchat2 - 2nd Force Sub Channel ID
+
+/totalreq - Total Req in 2 Fsub Channels
+
+/purge_one - Delete Requests (1st fsub)
+
+/purge_two - Delete Requests (2nd fsub) 
+
+/stats - Check Stats
+
+/ping - Bot Speed , Cpu etc
+
+/restart - Restart ✅
+
+/logs - bot logs txt"""
+    BUTER_TXT = """⍟ 𝗠𝘆 𝗡𝗮𝗺𝗲 : MR.Bean
+⍟ 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 : @heyboy2k04 
+⍟ 𝗠𝘆 𝗦𝗲𝗿𝘃𝗲𝗿 : Digital.Ocean
+
+✪ സിനിമകൾ ഇഷ്ടപ്പെടുന്നവർക്കും സിനിമ ഡൗൺലോഡ് ചെയ്യുന്നവർക്കും വേണ്ടിയുള്ള മാത്രം ബോട്ട് ആണിത് 🤩
+
+✫ 𝖸𝗈𝗎 𝖢𝖺𝗇 𝖲𝖾𝗇𝖽 𝖡𝗈𝗍 𝖠𝖽𝗆𝗂𝗇 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 𝖥𝗈𝗋 𝖴𝗉𝗅𝗈𝖺𝖽𝗂𝗇𝗀 𝖬𝗈𝗏𝗂𝖾𝗌, 𝖯𝖺𝗂𝖽 𝖯𝗋𝗈𝗆𝗈𝗍𝗂𝗈𝗇 𝖾𝗍𝖼"""
+    WHYER_TXT = """<b>✰ സിനിമയെ ഇഷ്ടപ്പെടുന്നവർക്കായി നിങ്ങൾക്ക് ഞങ്ങളുടെ സിനിമ കൂട്ടായ്മയിലേക്ക് (സിനിമ പ്രേമി) എല്ലാവരും ജോയിൻ ചെയ്യുക🤍
+    
+◍ <a href=https://t.me/+IJh-LnhpCUQwMjE1>𝐂𝐨𝐦𝐦𝐮𝐧𝐢𝐭𝐲 - 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a> ||(100𝗄 𝖣𝗋𝖾𝖺𝗆 🤗, 𝗒𝗈𝗎 𝗌𝗁𝗈𝗎𝗅𝖽 𝗌𝗎𝗉𝗉𝗈𝗋𝗍!🤎)||
+    
+➪ 𝖸𝗈𝗎 𝖼𝖺𝗇 𝖺𝗅𝗌𝗈 𝗎𝗌𝖾 𝗂𝗍 𝗂𝖿 𝗒𝗈𝗎 𝖺𝖽𝖽 𝗆𝖾 𝗍𝗈 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉✨
+
+◍ <a href=http://t.me/Mrbeanmoviebot?startgroup=true>𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩</a>
+    
+ᴥ︎︎︎ 𝖳𝗈 𝗀𝖾𝗍 𝗆𝗈𝗏𝗂𝖾𝗌, 𝗒𝗈𝗎 𝖼𝖺𝗇 𝗋𝖾𝗊𝗎𝖾𝗌𝗍 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉 𝗎𝗌𝗂𝗇𝗀 𝗍𝗁𝖾 𝖻𝗎𝗍𝗍𝗈𝗇 𝖻𝖾𝗅𝗈𝗐, 𝖿𝗈𝗋 𝗈𝗍𝗁𝖾𝗋 𝗍𝗁𝗂𝗇𝗀𝗌, 𝗒𝗈𝗎 𝖼𝖺𝗇 𝗌𝖾𝗇𝖽 𝖺 𝗆𝖾𝗌𝗌𝖺𝗀𝖾 𝗍𝗈 𝗍𝗁𝖾 𝖻𝗈𝗍 𝗀𝗋𝗈𝗎𝗉 𝖺𝖽𝗆𝗂𝗇.</b>"""
+    EXSERI_TXT = """Help: <b>Extra Modules</b>
+
+<b>NOTE:</b>
+these are the extra features of Mr.Bean
+
+<b>Commands and Usage:</b>
+• /id - <code>get id of a specified user.</code>
+• /info  - <code>get information about a user.</code>
+• /imdb  - <code>get the film information from IMDb source.</code>
+• /search  - <code>get the film information from various sources.</code>"""
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+
+- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+
+<b>NOTE:</b>
+1. eva maria should have admin privillage.
+2. only admins can add filters in a chat.
+3. alert buttons have a limit of 64 characters.
+
+<b>Commands and Usage:</b>
+• /filter - <code>add a filter in chat</code>
+• /filters - <code>list all the filters of a chat</code>
+• /del - <code>delete a specific filter in chat</code>
+• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+    SETIN_TXT = """Settings is most important features in this Bot
+You can easily customise this Bot for your group
+
+Note
+1. Only group admins can use this command and change settings
+2. It works only when you already connected your group
+
+Command and uses
+/settings - change settings as your wish"
+"""
+    BUTTON_TXT = """Help: <b>Buttons</b>
+
+- Eva Maria Supports both url and alert inline buttons.
+
+<b>NOTE:</b>
+1. Telegram will not allows you to send buttons without any content, so content is mandatory.
+2. Eva Maria supports buttons with any telegram media type.
+3. Buttons should be properly parsed as markdown format
+
+<b>URL buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+
+<b>Alert buttons:</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>"""
+    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+
+<b>NOTE:</b>
+1. Make me the admin of your channel if it's private.
+2. make sure that your channel does not contains camrips, porn and fake files.
+3. Forward the last message to me with quotes.
+ I'll add all the files in that channel to my db."""
+    CONNECTION_TXT = """Help: <b>Connections</b>
+
+- Used to connect bot to PM for managing filters 
+- it helps to avoid spamming in groups.
+
+<b>NOTE:</b>
+1. Only admins can add a connection.
+2. Send <code>/connect</code> for connecting me to ur PM
+
+<b>Commands and Usage:</b>
+• /connect  - <code>connect a particular chat to your PM</code>
+• /disconnect  - <code>disconnect from a chat</code>
+• /connections - <code>list all your connections</code>"""
+    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+
+<b>NOTE:</b>
+these are the extra features of Eva Maria
+
+<b>Commands and Usage:</b>
+• /id - <code>get id of a specified user.</code>
+• /info  - <code>get information about a user.</code>
+• /imdb  - <code>get the film information from IMDb source.</code>
+• /search  - <code>get the film information from various sources.</code>"""
+    ADMIN_TXT = """Help: <b>Admin mods</b>
+
+<b>NOTE:</b>
+This module only works for my admins
+
+<b>Commands and Usage:</b>
+• /logs - <code>to get the rescent errors</code>
+• /stats - <code>to get status of files in db.</code>
+• /delete - <code>to delete a specific file from db.</code>
+• /users - <code>to get list of my users and ids.</code>
+• /chats - <code>to get list of the my chats and ids </code>
+• /leave  - <code>to leave from a chat.</code>
+• /disable  -  <code>do disable a chat.</code>
+• /ban  - <code>to ban a user.</code>
+• /unban  - <code>to unban a user.</code>
+• /channel - <code>to get list of total connected channels</code>
+• /broadcast - <code>to broadcast a message to all users</code>"""
+    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT2 = """📂 𝖳𝗈𝗍𝖺𝗅 𝖥𝗂𝗅𝖾𝗌     - <code>{}</code>
+
+𝗗𝗕 𝟭
+𝖳𝗈𝗍𝖺𝗅 𝖥𝗂𝗅𝖾𝗌         - <code>{}</code>
+𝖴𝗌𝖾𝖽 𝖲𝗍𝗈𝗋𝖺𝗀𝖾   - <code>{}</code>MB
+𝖥𝗋𝖾𝖾 𝖲𝗍𝗈𝗋𝖺𝗀𝖾    - <code>{}</code>MB
+
+𝗗𝗕 𝟮
+𝖳𝗈𝗍𝖺𝗅 𝖥𝗂𝗅𝖾𝗌         - <code>{}</code>
+𝖴𝗌𝖾𝖽 𝖲𝗍𝗈𝗋𝖺𝗀𝖾   - <code>{}</code>MB
+𝖥𝗋𝖾𝖾 𝖲𝗍𝗈𝗋𝖺𝗀𝖾    - <code>{}</code>MB
+
+𝗗𝗕 𝟯
+📦 𝖴𝗌𝖾𝗋𝗌            - <code>{}</code>
+🖥️ 𝖢𝗁𝖺𝗍𝗌            - <code>{}</code>
+𝖴𝗌𝖾𝖽 𝖲𝗍𝗈𝗋𝖺𝗀𝖾   - <code>{}</code>MB
+𝖥𝗋𝖾𝖾 𝖲𝗍𝗈𝗋𝖺𝗀𝖾    - <code>{}</code>MB"""
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
+"""
+    CUSTOM_FILE_CAPTION = """<b><a href=https://t.me/+gOksHU6TBXM3Zjc1>{file_name}</a>"""
---
+++ bot.py
@@ -0,0 +1,149 @@
+import logging
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, REQ_CHANNEL1, REQ_CHANNEL2, LOG_CHANNEL
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from plugins.commands import restarti
+import os 
+import sys
+from dotenv import load_dotenv
+
+load_dotenv("./dynamic.env", override=True, encoding="utf-8")
+
+async def restart_bot(bot):
+    progress_document = restarti.find_one({"_id": "frestart"})
+    if progress_document:
+        last_restart = progress_document.get("restart")
+        if last_restart == "on":
+            restarti.update_one(
+                {"_id": "frestart"},
+                {"$set": {"restart": "off"}},
+                upsert=True
+            )
+            os.execl(sys.executable, sys.executable, "bot.py")
+        else:
+            return 
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
+    async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        await super().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
+        if REQ_CHANNEL1 == None:
+            with open("./dynamic.env", "wt+") as f:
+                req = await db.get_fsub_chat()                
+                if req is None:
+                    req = False
+                else:
+                    req = req['chat_id']                   
+                f.write(f"REQ_CHANNEL1={req}\n")
+                
+            logging.info("Loading REQ_CHANNEL from database...") 
+            os.execl(sys.executable, sys.executable, "bot.py")
+            return 
+        if REQ_CHANNEL2 == None:
+            with open("./dynamic.env", "wt+") as f:
+                req2 = await db.get_fsub_chat2()
+                if req2 is None:
+                    req2 = False
+                else:
+                    req2 = req2['chat_id']
+                f.write(f"REQ_CHANNEL2={req2}\n")
+            logging.info("Loading REQ_CHANNEL...") 
+            os.execl(sys.executable, sys.executable, "bot.py")
+            return 
+        await self.send_message(chat_id=int(6446790411), text="restarted ❤️‍🩹")
+
+        if REQ_CHANNEL1 != False:           
+            try:
+                _link = await self.create_chat_invite_link(chat_id=int(REQ_CHANNEL1), creates_join_request=True)
+                self.req_link1 = _link.invite_link
+            except Exception as e:
+                logging.info(f"Make Sure REQ_CHANNEL 1 ID is correct or {e}")
+        if REQ_CHANNEL2 != False:
+            try:
+                _link = await self.create_chat_invite_link(chat_id=int(REQ_CHANNEL2), creates_join_request=True)
+                self.req_link2 = _link.invite_link
+            except Exception as e:
+                logging.info(f"Make Sure REQ_CHANNEL 2 ID is correct or {e}")
+
+        await restart_bot(self)
+        
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped. Bye.")
+    
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                for message in app.iter_messages("pyrogram", 1, 15000):
+                    print(message.text)
+        """
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
+
+
+app = Bot()
+app.run()
