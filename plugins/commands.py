db1b763b1ed837c0f6d6c20ba0e1983329167d41
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-11-03 10:57:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -351,7 +351,7 @@ async def start(client, message):
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
                 try:
-                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption, mention=message.from_user.mention)
+                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption, mention=message.from_user.mention)    
                 except:
                     return
             await msg.edit_caption(f_caption)
