31931bedfbfeee46cb91b3fbf8f82e17d4dd87f8
tarzan1001 <adisha18086@gmail.com>
2024-11-14 21:56:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ pyroutils.MIN_CHAT_ID = -999999999999
 pyroutils.MIN_CHANNEL_ID = -100999999999999
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002276401285"))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002486076285"))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002354592029').split()]
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mcumovies')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
